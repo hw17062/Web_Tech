29650f279067f0da62a47dc80b4d984c610a2d4d
hw17062 <hw17062@bristol.ac.uk>
2020-06-05 22:34:00 +0100

    boxes is fullt functional at localhost/boxes

--- routes/boxes.js
+++ routes/boxes.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.sendFile('/public/boxes.html');
+router.get('/boxes', function(req, res, next) {
+  res.sendFile('boxes.html', { root: path.join(__dirname, '../public') });
 });
 
 module.exports = router;
--- routes/routerAll.js
+++ routes/routerAll.js
@@ -3,4 +3,5 @@ module.exports = function (app) {
   app.use('/', require('./auth'));
   app.use('/', require('./index'));
   app.use('/', require('./users'));
+  app.use('/', require('./boxes'));
 };
