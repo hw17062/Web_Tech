eef0b913bdb62019954807f661c9471e1f49611a
anndreea98 <lh17439@bristol.ac.uk>
2020-06-06 14:55:33 +0100

    Side bar and popup form implemented

--- public/javascripts/boxes.js
+++ public/javascripts/boxes.js
@@ -19,3 +19,19 @@ function createBox(){
       event.stopPropagation();
     })
 }
+
+function openNav() {
+      document.getElementById("mySidenav").style.width = "250px";
+     }
+
+function closeNav() {
+      document.getElementById("mySidenav").style.width = "0";
+}
+
+function openForm() {
+      document.getElementById("myForm").style.display = "block";
+    }
+
+function closeForm() {
+      document.getElementById("myForm").style.display = "none";
+}
