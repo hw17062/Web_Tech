57519a897096df302c41af2bc4d114c16fd095e7
anndreea98 <lh17439@bristol.ac.uk>
2020-06-06 12:28:30 +0100

    resize is working now

--- public/javascripts/boxes.js
+++ public/javascripts/boxes.js
@@ -4,10 +4,13 @@
 function createBox(){
 
     var number = Math.random() - .5;
-    $('<li>' + '</li>').attr('class', 'list').attr('id','slot'+ number).appendTo( '#items' ).sortable();
+    $('<li>' + '</li>').attr('class', 'list').attr('id','slot'+ number).appendTo( '#items' );
+    $(".list").resizable({
+              handles: 'se',
 
+          });
     var slot = document.getElementById('slot' + number);
-    $('<button>X</button>').attr('id','close'+number).appendTo(slot);
+    $('<button>X</button>').attr('class', 'btn').attr('id','close'+number).appendTo(slot);
 
 
     var closeBtn = document.getElementById('close' + number);
