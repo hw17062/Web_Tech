a391201369886d16ac847882c0e4337d9cce8a28
anndreea98 <lh17439@bristol.ac.uk>
2020-06-07 18:24:32 +0100

    m

--- public/javascripts/boxes.js
+++ public/javascripts/boxes.js
@@ -15,47 +15,9 @@ document.getElementById("tabName").innerHTML = currentTab;
 $('<button>'+currentTab+'</button>').attr('id', 'tab' + currentTab).attr('class', 'tab').click(function () {changeTab("Home")}).appendTo('#mySidenav');
 
 
-<<<<<<< HEAD
-    $('<li>' + '</li>').attr('class', 'bookmark').attr('id','slot'+ number).appendTo('#items');
-    $(".bookmark").resizable({
-              handles: 'se',
-
-          });
-    document.getElementById('slot' + number).style.background = getRandomColor();
-    var slot = document.getElementById('slot' + number);
-
-    $('<button>X</button>').attr('class', 'bookmarkClose').attr('id','close'+number).appendTo(slot);
-
-
-    var closeBtn = document.getElementById('close' + number);
-    $(closeBtn).click(function () {
-      $(slot).remove();
-      event.stopPropagation();
-    })
-
-    $('<a href="'+text+'" target="_blank">'+ 'Go to page' + '</a>').attr('class', 'bookmarkLink').attr('id', 'pagelink' + number).appendTo(slot);
-
-    var link = document.getElementById('pagelink' + number);
-    $('<img src="' + text + '/favicon.ico" idth="200" height="40">').appendTo(link);
-
-    closeBookmarkForm();
-
-  }
-
-
-
-function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-=======
 window.newBookmark = function newBookmark(){
   var link = document.getElementById("nameBookmark").value;
   tabs[currentTab].createBox(link, '#ddd');
->>>>>>> 7eaf643fcfb218edd89141b6a58dd1c1ede11dc5
 
   closebookmarkForm()
 }
