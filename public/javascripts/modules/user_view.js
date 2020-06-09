5b52f322d95bd33afb7c38bfc25b6ffe4e06d0ef
hw17062 <hw17062@bristol.ac.uk>
2020-06-09 12:29:45 +0100

    creating a json string of user's tabs

--- public/javascripts/modules/user_view.js
+++ public/javascripts/modules/user_view.js
@@ -14,13 +14,34 @@ import Tab from "./tab.js";
 export default class user_view{
   constructor(){
     this.tabs = []
+    this.jsonString = ""
   }
 
   newTab(name){
     this.tabs.push(new Tab(name));
+    this.toJSON();
   }
 
   removeTab(name){
     delete this.tabs[name];
   }
+
+  // This will take the current user's layout and convert it into JSON
+  toJSON(){
+    this.jsonString = JSON.stringify(this.tabs, this.getCircularReplacer());
+    console.log(this.jsonString);
+  }
+
+  getCircularReplacer(){
+  const seen = new WeakSet();
+  return (key, value) => {
+    if (typeof value === "object" && value !== null) {
+      if (seen.has(value)) {
+        return;
+      }
+      seen.add(value);
+    }
+    return value;
+  };
+};
 }
